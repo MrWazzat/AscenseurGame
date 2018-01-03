264ac1f13f769c1e3eab51aadeccf09c16bb7158
MrWazzat <raphael.marzat@outlook.com>
2018-01-03 12:26:56 +0100

    Update Main.cs

--- AscenseurGame/Main.cs
+++ AscenseurGame/Main.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//YOU HAVE BEEN VISITED BY THE GOOD BOI PROPHET, MAY REST BE UPON YOU.
 namespace AscenseurGame
 {
     class Main
